01a6cc792f4b37eb21c437ec477fafbacdc1ae18
michaels <michael.sebald@uniserv.com>
2018-01-09 12:10:48 +0100

    improved graphics

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 library(shinydashboard)
 library(readr)
 library(shinyTime)
+library(dplyr)
+library(ggplot2)
 
 options(shiny.maxRequestSize=30*1024^2)
 
@@ -63,8 +65,13 @@ ui <- dashboardPage(
         fluidRow(
           plotOutput("timetakenHist")
         ),
+        br(),
         fluidRow(
           plotOutput("reqtypePlot")
+        ),
+        br(),
+        fluidRow(
+          plotOutput("reqperfPlot")
         )
       # not working with second tabItem. why???  
       # ),
@@ -82,11 +89,11 @@ ui <- dashboardPage(
 server <- function(input, output, session) {
   dataInput <- reactive({
     cat("Reading file...")
-    if (is.null(input$file))
-      return(NULL)
-
-    iis <- read_delim(input$file$datapath, " ",
-    # iis <- read_delim("../iislogging_data/iis.log", " ",
+    
+    # if (is.null(input$file))
+    #   return(NULL)
+    # iis <- read_delim(input$file$datapath, " ",
+    iis <- read_delim("../iislogging_data/iis.log", " ",
                       escape_double = FALSE, col_names = FALSE, trim_ws = TRUE,
                       col_types = paste0(rep("c", 15), collapse = ""),
                       na = "-", comment = "#")
@@ -245,30 +252,52 @@ server <- function(input, output, session) {
   output$timetakenHist <- renderPlot({
     x <- datasetUpdate()
     if (!is.null(x)) {
-      hist(
-        as.numeric(x$timetaken),
-        main = "Distribution of time-taken",
-        xlab = "time-taken interval [secs]",
-        ylab = "Number of requests per interval",
-        col = "#75AADB",
-        border = "white"
-      )
+      p <- ggplot(x, aes(x = timetaken, fill = "#75AADB")) +
+        ggtitle("Distribution of time-taken") + 
+        xlab("time-taken interval [secs]") +
+        ylab("Number of requests per interval") +
+        geom_histogram()
+      p
     }
   })
 
   output$reqtypePlot <- renderPlot({
     x <- datasetUpdate()
     if (!is.null(x)) {
-      barplot(
-        table(x$reqtype),
-        main = "Request types",
-        xlab = "Request type",
-        ylab = "Number of requests per type",
-        col = "#75AADB",
-        border = "white"
-      )
+      df <- as.data.frame(table(x$reqtype))
+      colnames(df) <- c("Request type", "Frequency")
+      p <- ggplot(df, aes(x = `Request type`, 
+                          y = Frequency, 
+                          fill = `Request type`)) +
+        geom_bar(stat = "identity") +
+        ggtitle("Number of requests per type") +
+        xlab("Request type") +
+        ylab("Number of requests per type")
+      p
     }
   })
+  
+  output$reqperfPlot <- renderPlot({
+    x <- datasetUpdate()
+    if (!is.null(x)) {
+      reqperf <- x %>%
+        mutate(window = cut(timestamp, "min")) %>%
+        group_by(window) %>%
+        summarise(requests = n())
+
+      reqperf$window <- as.POSIXct(reqperf$window)
+      
+      print(reqperf)
+      p <- ggplot(reqperf, aes(x = reqperf$window, y = reqperf$requests)) +
+        geom_line() +
+        scale_x_datetime() +
+        ggtitle("Requests per minute") +
+        xlab("Time") + 
+        ylab("Number of requests")
+      p
+    }
+  })
+  
 }
 
 # Run the application
